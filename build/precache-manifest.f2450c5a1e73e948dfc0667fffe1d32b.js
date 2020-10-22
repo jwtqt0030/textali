274031c504ae1675f69f252cc98f58687ffdf343
jwtqt0030 <jwtqt0030@163.com>
2020-10-22 16:25:12 +0800

    yes

---
+++ build/precache-manifest.f2450c5a1e73e948dfc0667fffe1d32b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cdcc97059da9cfc55c0ebec4e5a4958",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88e58ff5a7c38522923a",
+    "url": "/static/css/2.2f75dd9a.chunk.css"
+  },
+  {
+    "revision": "596e185cf6dae8526561",
+    "url": "/static/css/main.f40be5f8.chunk.css"
+  },
+  {
+    "revision": "88e58ff5a7c38522923a",
+    "url": "/static/js/2.9a3bb4b7.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/static/js/2.9a3bb4b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "596e185cf6dae8526561",
+    "url": "/static/js/main.8173b291.chunk.js"
+  },
+  {
+    "revision": "27763ce3a1918c502fb7",
+    "url": "/static/js/runtime-main.a5d7d421.js"
+  }
+]);
