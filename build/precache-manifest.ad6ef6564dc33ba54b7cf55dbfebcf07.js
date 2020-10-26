9a173913dcc30480992b2efcf9d9310c5b67b351
jwtqt0030 <jwtqt0030@163.com>
2020-10-26 15:28:53 +0800

    yes

---
+++ build/precache-manifest.ad6ef6564dc33ba54b7cf55dbfebcf07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a1885f004cd3d897a200f4ec3efdcc",
+    "url": "/textail/index.html"
+  },
+  {
+    "revision": "88e58ff5a7c38522923a",
+    "url": "/textail/static/css/2.2f75dd9a.chunk.css"
+  },
+  {
+    "revision": "65311c3f4fdd7ad275e8",
+    "url": "/textail/static/css/main.f40be5f8.chunk.css"
+  },
+  {
+    "revision": "88e58ff5a7c38522923a",
+    "url": "/textail/static/js/2.9a3bb4b7.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/textail/static/js/2.9a3bb4b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65311c3f4fdd7ad275e8",
+    "url": "/textail/static/js/main.031ff155.chunk.js"
+  },
+  {
+    "revision": "d67cb29b049e60a0539a",
+    "url": "/textail/static/js/runtime-main.91212e35.js"
+  }
+]);
